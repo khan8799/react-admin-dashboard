daf5781fa635d93279c1a3a39446abe8863945b3
Mohammad Farman <farmankhanmlc@gmail.com>
2024-05-27 12:13:47 +0530

    code refactoring

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -23,18 +23,17 @@ function Home() {
         }
     ]
 
+    const dummyArr = [1, 1, 1, 1];
+
     return (
         <>
             <Breadcrumb name="Dashboard" />
 
             <div className="row">
-            
                 {
                     appSummary.map((summary, i) => <AppSummary key={i} summary={summary}/>)
                 }
-                
             </div>
-            
 
             <div className="row">
                 <div className="col-md-7 grid-margin stretch-card">
@@ -76,50 +75,25 @@ function Home() {
                                 </tr>
                             </thead>
                             <tbody>
-                            <tr>
-                                <td>
-                                <img src="assets/images/faces/face1.jpg" className="me-2" alt="image" /> David Grey
-                                </td>
-                                <td> Fund is not recieved </td>
-                                <td>
-                                <label className="badge badge-gradient-success">DONE</label>
-                                </td>
-                                <td> Dec 5, 2017 </td>
-                                <td> WD-12345 </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                <img src="assets/images/faces/face2.jpg" className="me-2" alt="image" /> Stella Johnson
-                                </td>
-                                <td> High loading time </td>
-                                <td>
-                                <label className="badge badge-gradient-warning">PROGRESS</label>
-                                </td>
-                                <td> Dec 12, 2017 </td>
-                                <td> WD-12346 </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                <img src="assets/images/faces/face3.jpg" className="me-2" alt="image" /> Marina Michel
-                                </td>
-                                <td> Website down for one week </td>
-                                <td>
-                                <label className="badge badge-gradient-info">ON HOLD</label>
-                                </td>
-                                <td> Dec 16, 2017 </td>
-                                <td> WD-12347 </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                <img src="assets/images/faces/face4.jpg" className="me-2" alt="image" /> John Doe
-                                </td>
-                                <td> Loosing control on server </td>
-                                <td>
-                                <label className="badge badge-gradient-danger">REJECTED</label>
-                                </td>
-                                <td> Dec 3, 2017 </td>
-                                <td> WD-12348 </td>
-                            </tr>
+                                {
+                                    dummyArr.map((v, i) => {
+                                        return (
+                                            <>
+                                                <tr key={i + 1}>
+                                                    <td>
+                                                    <img src="assets/images/faces/face1.jpg" className="me-2" alt="image" /> David Grey
+                                                    </td>
+                                                    <td> Fund is not recieved </td>
+                                                    <td>
+                                                    <label className="badge badge-gradient-success">DONE</label>
+                                                    </td>
+                                                    <td> Dec 5, 2017 </td>
+                                                    <td> WD-12345 </td>
+                                                </tr>
+                                            </>
+                                        )
+                                    })
+                                }
                             </tbody>
                         </table>
                         </div>
