2f6a115dabf227c1e2c7e31c3cd7e2569c3c69d7
Mohammad Farman <farmankhanmlc@gmail.com>
2024-07-10 17:23:19 +0530

    delete brand

--- src/Brand/BrandList.js
+++ src/Brand/BrandList.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import Table from '../components/Table';
 import { makeRequest } from './../shared/utilities/httpHelper'
+import Alert from '../shared/components/Alert/Alert';
+import { useSnackbar } from 'notistack';
 
 export default function BrandList({ toggleLoading, newBrand }) {
     const [brands, setBrands] = useState([]);
+    const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+    let selectedBrand;
 
     useEffect(() => getBrands(), [newBrand])
 
@@ -14,14 +18,56 @@ export default function BrandList({ toggleLoading, newBrand }) {
             .finally(() => toggleLoading(false))
     }
 
+    const action = () => (
+        <>
+            <button className="btn btn-sm btn-danger me-2" onClick={() => {
+                deleteBrand()
+                closeSnackbar()
+            }}>
+                Yes, Delete it
+            </button>
+            <button className="btn btn-sm btn-primary" onClick={closeSnackbar}>
+                No
+            </button>
+        </>
+    );
+
+    const showConfirm = (brand) => {
+        selectedBrand = brand
+        enqueueSnackbar(`Do you really want to delete ${brand.name}`, {
+            action,
+            variant: 'error',
+            anchorOrigin: { vertical: 'top', horizontal: 'center' }
+        })
+    }
+
+    const deleteBrand = () => {
+        toggleLoading(true)
+
+        const url = `brand/${selectedBrand._id}`
+        const option = {
+            method: 'DELETE',
+        }
+        makeRequest(url, option)
+            .then(res => removeDeletedBrandFromList())
+            .finally(() => toggleLoading(false))
+    }
+
+    const removeDeletedBrandFromList = () => {
+        const newBrands = brands.filter(brand => brand._id !== selectedBrand._id)
+        setBrands(newBrands)
+    }
+
     return (
         <>
+            <Alert />
             <Table>
                 <thead>
                     <tr>
                         <th> # </th>
                         <th> Name </th>
                         <th> Description </th>
+                        <th>  </th>
                     </tr>
                 </thead>
                 <tbody>
@@ -30,7 +76,28 @@ export default function BrandList({ toggleLoading, newBrand }) {
                             <tr key={ index + 1 }>
                                 <td> { index + 1 } </td>
                                 <td> { brand.name } </td>
-                                <td> { brand.description } </td>
+                                <td>
+                                    <div  className="w-100 ">
+                                        { brand.description } 
+                                    </div>
+                                </td>
+                                <td>
+                                    <div className='template-demo d-flex justify-content-between flex-nowrap'>
+                                        <button
+                                            type="button"
+                                            className="btn btn-inverse-danger btn-rounded btn-icon"
+                                            onClick={() => showConfirm(brand)}>
+                                                <i className="mdi menu-icon mdi-trash-can"></i>
+                                        </button>
+
+                                        <button
+                                            type="button"
+                                            className="btn btn-inverse-success btn-rounded btn-icon"
+                                            onClick={showConfirm}>
+                                                <i className="mdi menu-icon mdi-content-save-edit"></i>
+                                        </button>
+                                    </div>
+                                </td>
                             </tr>
                         );
                     })}
--- src/Layout.js
+++ src/Layout.js
@@ -2,24 +2,27 @@ import Header from './shared/components/Header/Header'
 import Sidebar from './shared/components/Sidebar/Sidebar'
 import { Outlet } from 'react-router-dom';
 import Footer from './shared/components/Footer/Footer'
+import { SnackbarProvider } from 'notistack';
 
 function Layout() {
   return (
     <>
-      <Header />
+      <SnackbarProvider>
+        <Header />
 
-      <div className="container-fluid page-body-wrapper">
-        <Sidebar />
-        
-        <div className="main-panel">
-            <div className="content-wrapper">
-				      <Outlet />
-            </div>
+        <div className="container-fluid page-body-wrapper">
+          <Sidebar />
+          
+          <div className="main-panel">
+              <div className="content-wrapper">
+                <Outlet />
+              </div>
 
-            <Footer />
+              <Footer />
+          </div>
+
         </div>
-
-      </div>
+      </SnackbarProvider>
     </>
   );
 }
