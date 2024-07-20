5211c00c7b9037f418742bdde2b61bb0489bc4c1
Mohammad Farman <farmankhanmlc@gmail.com>
2024-07-20 11:49:37 +0530

    WIP: coupon module with list, add, edit and delete functionality

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import Home from './Home/Home'
 import CategoryList from './Category/CategoryList/CategoryList'
 import ProductList from './Product/ProductList/ProductList';
 import Brand from './Brand/Brand';
+import Coupon from './Coupon/Coupon';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,6 +18,7 @@ root.render(
         <Route index element={<Home />}></Route>
         <Route path="category" element={<CategoryList />}></Route>
         <Route path="brand" element={<Brand />}></Route>
+        <Route path="coupon" element={<Coupon />}></Route>
         <Route path="product" element={<ProductList />}></Route>
       </Route>
       <Route path="/login" element={<Login />}></Route>
