fb7c275e12c25d29bc016e6a91e82ad1b6a10fd8
amil usman <amilusman8577988622@gmail.com>
2024-08-03 13:44:24 +0530

    Merge branch 'feat/product-add-product-form' of https://github.com/khan8799/react-admin-dashboard into feat/product-add

---
+++ src/Branch/BranchList.js
@@ -0,0 +1,72 @@
+import { useState, useEffect } from "react";
+import Table from "../components/Table";
+import { makeRequest } from "../shared/utilities/httpHelper";
+
+export default function BranchList( {toggleLoading} ) {
+    const [branches, setBranches] = useState([])
+    useEffect(() => getBranches(), [])
+
+    const getBranches = () => {
+        toggleLoading(true)
+        const url = `branch`
+        makeRequest(url)w``
+            .then(res => {
+                setBranches(res.payload)
+            })
+            .finally(() => toggleLoading(false))
+    }
+    const onDelete = () => {
+        console.log('delete');
+    }
+    return (
+        <>
+            <Table>
+                <thead>
+                    <tr>
+                        <th> # </th>
+                        <th> Name </th>
+                        <th> Latitude </th>
+                        <th> Longitude</th>
+                        <th> </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {branches.map((branch, index) => {
+                        return (
+                            <tr key={index + 1}>
+                                <td> {index + 1} </td>
+                                <td>{branch.name}  </td>
+                                <td>
+                                    <div className="w-100 ">
+                                        {branch.location.latitude}
+                                    </div>
+                                </td>
+                                <td>
+                                <div className="w-100 ">
+                                        {branch.location.longitude}
+                                    </div>
+                                </td>
+                                <td>
+                                    <div className="template-demo d-flex justify-content-between flex-nowrap">
+                                        <button
+                                            type="button"
+                                            className="btn btn-inverse-danger btn-rounded btn-icon"
+                                            onClick={() => onDelete(branch)}>
+                                            <i className="mdi mdi-trash-can"></i>
+                                        </button>
+                                        <button
+                                            type="button"
+                                            className="btn btn-inverse-success btn-rounded btn-icon"
+                                            >
+                                            <i className="mdi mdi-content-save-edit"></i>
+                                        </button>
+                                    </div>
+                                </td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </Table>
+        </>
+    )
+}
